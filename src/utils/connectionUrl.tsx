3be64ffa4c8d1bdd7f174ccee3c66416d88bca3e
jashanpreet <jashanpreet28122001@gmail.com>
2025-07-04 13:10:23 -0500

    hotfix/ ip address in env added

--- src/utils/connectionUrl.tsx
+++ src/utils/connectionUrl.tsx
@@ -1,3 +1,6 @@
 //export const connectionUrl = "https://visualization-backend-oi6i.onrender.com";
-//export const connectionUrl = "http://localhost:3001";
-export const connectionUrl =  "http://172.105.98.10:3000";
+//export const connectionUrl = "http://localhost:3000";
+//export const connectionUrl =  "https://172.105.98.10:3000";
+//export const connectionUrl = "http://172.105.98.10:3000";
+export const connectionUrl = import.meta.env.VITE_API_CONNECTION_URL as string;
+console.log("Connection URL:", connectionUrl);
