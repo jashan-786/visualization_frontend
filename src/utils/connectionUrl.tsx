7eecc1feb91511349c0a0a2df2bc0d4bff1349db
jashanpreet <jashanpreet28122001@gmail.com>
2025-07-04 12:44:55 -0500

    cloud/ip added

--- src/utils/connectionUrl.tsx
+++ src/utils/connectionUrl.tsx
@@ -1,2 +1,3 @@
 //export const connectionUrl = "https://visualization-backend-oi6i.onrender.com";
-export const connectionUrl = "http://localhost:3000";
+//export const connectionUrl = "http://localhost:3001";
+export const connectionUrl =  "http://172.105.98.10:3000";
